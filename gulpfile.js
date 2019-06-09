88de64677759bc63f055d28a5e8c7da4ecb8da6a
vitorregisr <vitorregisrr@gmail.com>
2019-06-09 15:17:17 -0300

    Improve navbar, Improve home, Improve styles, Add Sobre page, Misc improves and fixies

--- gulpfile.js
+++ gulpfile.js
@@ -154,7 +154,7 @@ gulp.task('copyRecursiveVendorFiles', function () {
 /*Building vendor scripts needed for basic template rendering*/
 gulp.task('buildBaseVendorScripts', function () {
     return gulp
-        .src(['./node_modules/jquery/dist/jquery.min.js', './node_modules/wowjs/dist/wow.min.js', './node_modules/lazyload/lazyload.js', './node_modules/owl.carousel/dist/owl.carousel.min.js', './node_modules/jquery-validation/dist/jquery.validate.js', './node_modules/jquery-bar-rating/dist/jquery.barrating.min.js', './node_modules/lightbox2/dist/js/lightbox.min.js', './node_modules/jquery-mask-plugin/dist/jquery.mask.min.js'])
+        .src(['./node_modules/jquery/dist/jquery.min.js', './node_modules/wowjs/dist/wow.min.js', './node_modules/lazyload/lazyload.js', './node_modules/owl.carousel/dist/owl.carousel.min.js', './node_modules/jquery-validation/dist/jquery.validate.js', './node_modules/jquery-bar-rating/dist/jquery.barrating.min.js', './node_modules/lightbox2/dist/js/lightbox.min.js', './node_modules/jquery-mask-plugin/dist/jquery.mask.min.js', './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'])
         .pipe(concat('vendor.bundle.base.js'))
         .pipe(gulp.dest('./vendors/js'));
 });
--- js/responsivenav.js
+++ js/responsivenav.js
@@ -1,7 +1,25 @@
 (function () {
     'use strict';
 
-    $('#navToggler').click( function(){
+    $('#navToggler').click(function () {
         $('#main-nav').toggleClass('active');
     });
+
+    // $('.dropdown').find('a').click();
+    // $('.dropdown').find('a').click();
+
+    // function toggleDropdown (e) {
+    //     const _d = $(e.target).closest('.dropdown'),
+    //       _m = $('.dropdown-menu', _d);
+    //     setTimeout(function(){
+    //       const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    //       _m.toggleClass('show', shouldOpen);
+    //       _d.toggleClass('show', shouldOpen);
+    //       $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+    //     }, e.type === 'mouseleave' ? 300 : 0);
+    //   }
+      
+    //   $('body')
+    //     .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    //     .on('click', '.dropdown-menu a', toggleDropdown);
 })();
