8e0e8bf5de2cee4f011b09b98e8655d77ae86794
vitorregisr <vitorregisrr@gmail.com>
2019-06-24 16:02:29 -0300

    Add mult-language to agenda date picker

--- js/inputmasks.js
+++ js/inputmasks.js
@@ -1,25 +1,36 @@
 (function () {
     'use strict';
 
-    $('input.daterange').daterangepicker({
-        locale: {
-            daysOfWeek: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
-            monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-            today: "Hoje",
-            clear: "Limpar",
-            format: "DD-MM-YYYY",
-            titleFormat: "MM yyyy",
-            weekStart: 0,
-            "separator": " - ",
-            "applyLabel": "Aplicar",
-            "cancelLabel": "Cancelar",
-            "fromLabel": "De",
-            "toLabel": "Até",
-        },
-        startDate: false
+    // CONFIGURAÇÃO BOOTSTRAP DATE RANGE PICKER (PÁGINA AGENDA) //
 
+    const languageVal = $('#languageValue');
+    let dateLocale = false;
+
+    if (languageVal.length > 0) {
+        if (languageVal.val() === 'pt') {
+            dateLocale = {
+                daysOfWeek: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+                monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+                today: "Hoje",
+                clear: "Limpar",
+                format: "DD-MM-YYYY",
+                titleFormat: "MM yyyy",
+                weekStart: 0,
+                "separator": " - ",
+                "applyLabel": "Aplicar",
+                "cancelLabel": "Cancelar",
+                "fromLabel": "De",
+                "toLabel": "Até",
+            };
+
+        }
+    }
+
+    $('input.daterange').daterangepicker({
+        locale: dateLocale,
+        startDate: false
     });
 
     $('input.daterange').val('');
-    $('input.daterange').attr("placeholder","Selecione um período");
+    $('input.daterange').attr("placeholder", "Selecione um período");
 })();
